6750b6fb34ce552eae86f287632a97d94cf72e99
Daniel Jones <danielkjones0@gmail.com>
2017-06-26 19:08:35 -0400

    Added design credit to footer

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <?php
     echo "<footer class=\"footer\">
                       <div class=\"container-fluid\">
-                          <div class=\"row\">
+                          <div class=\"row baseline\">
                               <div class=\"col-sm-6\">&copy; Bradford Evangelical Free Church 2017</div>
                               <div class=\"col-sm-4\">
                                   <a href=\"https://www.google.com/maps/place/Bradford+Evangelical+Free+Church/@43.9763418,-72.1203587,17z/data=!3m1!4b1!4m5!3m4!1s0x4cb4ecc71a67ff6f:0xee00f4305793d5b9!8m2!3d43.976338!4d-72.11817\" target=\"_blank\">
@@ -9,6 +9,12 @@
                                   </a>
                               </div>
                               <div class=\"col-sm-2\">(802) 222-9021</div>
+                          </div>
+                          <div class=\"row credit\">
+                              <div class=\"col-sm-9\"></div>
+                              <div class=\"col-sm-3\">
+                                  Web Design by Daniel Jones
+                              </div>
                           </div>
                       </div>
                   </footer>";
